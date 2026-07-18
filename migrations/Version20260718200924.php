abcb38bffdc2b31fd80ab1f046e27f1f8e596c69
Varga Tamas <vargata1@gmail.com>
2026-07-18 22:11:27 +0200

    Switch database from SQLite to MySQL running in Docker

--- migrations/Version20260718200924.php
+++ migrations/Version20260718200924.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260718195655 extends AbstractMigration
+final class Version20260718200924 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20260718195655 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE review (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, company_name VARCHAR(255) NOT NULL, rating INTEGER NOT NULL, review_text CLOB NOT NULL, author_email VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL)');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, company_name VARCHAR(255) NOT NULL, rating INT NOT NULL, review_text LONGTEXT NOT NULL, author_email VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
